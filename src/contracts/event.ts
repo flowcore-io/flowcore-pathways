e20e8f81a9cf68eaa598be5d43ef34f346f136ea
René Bischof <rene.bischoff@gmail.com>
2025-05-23 12:58:53 +0200

    feat: switch from typebox to zod

--- src/contracts/event.ts
+++ src/contracts/event.ts
@@ -1,31 +1,11 @@
-import { type Static, type TObject, type TRecord, type TString, type TUnknown, Type } from "@sinclair/typebox"
-
-/**
- * The schema for an event
- */
-export const FlowcoreEventSchema: TObject<{
-  eventId: TString
-  timeBucket: TString
-  tenant: TString
-  dataCoreId: TString
-  flowType: TString
-  eventType: TString
-  metadata: TRecord<TString, TUnknown>
-  payload: TRecord<TString, TUnknown>
-  validTime: TString
-}> = Type.Object({
-  eventId: Type.String(),
-  timeBucket: Type.String(),
-  tenant: Type.String(),
-  dataCoreId: Type.String(),
-  flowType: Type.String(),
-  eventType: Type.String(),
-  metadata: Type.Record(Type.String(), Type.Unknown()),
-  payload: Type.Record(Type.String(), Type.Unknown()),
-  validTime: Type.String(),
-})
-
-/**
- * The type for an event
- */
-export type FlowcoreEvent = Static<typeof FlowcoreEventSchema>
+export interface FlowcoreEvent<Payload = unknown, Metadata = Record<string, unknown>> {
+  eventId: string
+  timeBucket: string
+  tenant: string
+  dataCoreId: string
+  flowType: string
+  eventType: string
+  metadata: Metadata
+  payload: Payload
+  validTime: string
+}
